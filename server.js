c5e4850816c1d33126b12fe44426b1bfc595162f
Arslonbek25 <amadyarov25@gmail.com>
2025-04-29 09:12:02 -0400

    chore: scaffold server (#1)

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1>Mini App</h1>');
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+
